6217461a4317a372c891a3c70aa0b08fd9d0f58f
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2025-02-25 10:25:53 +0600

    implemented dark/light theme switcher

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -5,13 +5,13 @@ import React, {ReactNode} from 'react'
 const Layout = ({children} : {children : ReactNode}) => {
   return (
     <div className='flex flex-col min-h-screen min-w-full bg-background max-h-screen'>
-      <nav>
+      <nav className='flex justify-between items-center border-b border-border h-[60px] px-4 py-2'>
         <Logo/>
         <ThemeSwitcher/>
         <UserButton />
       </nav>
       <main className='flex w-full flex-grow'>{children}</main>
-      </div>
+    </div>
   )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/ThemeProvider";
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "FlexiForm",
@@ -24,21 +14,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider afterSignOutUrl="/sign-in">
-      <html lang="en">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <body
-            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-          >
-            {children}
-          </body>
-        </ThemeProvider>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className="antialiased">
+        <ClerkProvider afterSignOutUrl="/sign-in">
+          <ThemeProvider>{children}</ThemeProvider>
+        </ClerkProvider>
+      </body>
+    </html>
   );
 }
+
