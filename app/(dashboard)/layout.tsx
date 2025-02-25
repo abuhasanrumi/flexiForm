73e8e333b733c7b884237bd9c3ee9d3cc148a829
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2025-02-25 09:21:10 +0600

    initializing

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,18 @@
+import Logo from '@/components/Logo'
+import ThemeSwitcher from '@/components/ThemeSwitcher'
+import { UserButton } from '@clerk/nextjs'
+import React, {ReactNode} from 'react'
+const Layout = ({children} : {children : ReactNode}) => {
+  return (
+    <div className='flex flex-col min-h-screen min-w-full bg-background max-h-screen'>
+      <nav>
+        <Logo/>
+        <ThemeSwitcher/>
+        <UserButton />
+      </nav>
+      <main className='flex w-full flex-grow'>{children}</main>
+      </div>
+  )
+}
+
+export default Layout
