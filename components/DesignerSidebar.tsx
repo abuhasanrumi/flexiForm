39871bc23b19b5421b9ccc6990814baea55af5fe
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2025-04-13 01:57:20 +0600

    refactor(DesignerSidebar): replace static elements with dynamic sidebar components
    The DesignerSidebar component was refactored to dynamically display either the FormElementsSidebar or PropertiesFormSidebar based on the selected element. This improves the component's flexibility and aligns with the application's design requirements.

--- components/DesignerSidebar.tsx
+++ components/DesignerSidebar.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import { FormElements } from './FormElements'
-import SidebarBtnElement from './SidebarBtnElement'
+import FormElementsSidebar from './FormElementsSidebar'
+import useDesigner from './hooks/useDesigner'
+import PropertiesFormSidebar from './PropertiesFormSidebar'
 
 export default function DesignerSidebar() {
+  const { selectedElement } = useDesigner()
   return (
     <aside className='w-[400px] max-w-[400px] flex flex-col flex-grow gap-2 border-l-2 border-muted p-4 bg-background overflow-y-auto h-full'>
-      Elements
-      <SidebarBtnElement formElement={FormElements.TextField} />
+      {!selectedElement && <FormElementsSidebar />}
+      {selectedElement && <PropertiesFormSidebar />}
     </aside>
   )
 }
