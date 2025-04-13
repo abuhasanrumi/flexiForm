bad47746a10f152a3335b801b9bdf94b7e0229a5
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2025-04-14 03:37:10 +0600

    feat(PublishFormBtn): add confirmation dialog and publish functionality
    Implement a confirmation dialog before publishing a form to prevent accidental submissions. Add loading state and toast notifications for better user feedback. The form ID is now passed as a prop to enable the publish action.

--- components/PublishFormBtn.tsx
+++ components/PublishFormBtn.tsx
@@ -1,12 +1,66 @@
-import React from 'react'
-import { Button } from './ui/button'
+import { PublishForm } from '@/actions/form'
+import { useRouter } from 'next/navigation'
+import { useTransition } from 'react'
+import { ImSpinner2 } from 'react-icons/im'
 import { MdOutlinePublish } from 'react-icons/md'
+import { toast } from 'sonner'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger
+} from './ui/alert-dialog'
+import { Button } from './ui/button'
 
-export default function PublishFormBtn() {
+export default function PublishFormBtn({ id }: { id: number }) {
+  const [loading, startTransition] = useTransition()
+  const router = useRouter()
+  async function publishForm() {
+    try {
+      await PublishForm(id)
+      toast.success('Form published successfully')
+      router.refresh()
+    } catch (error) {
+      toast.error('Something went wrong', error)
+    }
+  }
   return (
-    <Button className='gap-2 text-white bg-gradient-to-r from-indigo-400 to-cyan-400'>
-      <MdOutlinePublish className='h-4 w-4' />
-      Publish
-    </Button>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button className='gap-2 text-white bg-gradient-to-r from-indigo-400 to-cyan-400'>
+          <MdOutlinePublish className='h-4 w-4' />
+          Publish
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. After publishing you will not be able
+            to edit this form. <br /> <br />
+            <span className='font-medium'>
+              By publishing this form you will make it available to the public
+              and you will be able to collect submission
+            </span>
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            disabled={loading}
+            onClick={(e) => {
+              e.preventDefault()
+              startTransition(publishForm)
+            }}>
+            Proceed {loading && <ImSpinner2 className='animate-spin' />}
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   )
 }
