52f5fcb9c4bd33ebb68718c2ea39018a8bcce4c2
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2025-04-14 03:37:22 +0600

    feat(SaveFormBtn): add form saving functionality with loading state
    Implement form saving functionality in SaveFormBtn component using UpdateFormContent action. Add loading state and transition handling to improve user experience during form submission.

--- components/SaveFormBtn.tsx
+++ components/SaveFormBtn.tsx
@@ -1,12 +1,35 @@
-import React from 'react'
+import { UpdateFormContent } from '@/actions/form'
+import { useTransition } from 'react'
+import { FaSpinner } from 'react-icons/fa'
+import { HiSaveAs } from 'react-icons/hi'
+import { toast } from 'sonner'
+import useDesigner from './hooks/useDesigner'
 import { Button } from './ui/button'
-import {HiSaveAs} from 'react-icons/hi'
 
-export default function SaveFormBtn() {
+export default function SaveFormBtn({ id }: { id: number }) {
+  const { elements } = useDesigner()
+  const [loading, startTransition] = useTransition()
+
+  const updateFormContent = async () => {
+    try {
+      const JSONElements = JSON.stringify(elements)
+      await UpdateFormContent(id, JSONElements)
+      toast.success('Form saved successfully')
+    } catch (error) {
+      toast.error('Something went wrong', error)
+    }
+  }
   return (
-    <Button variant={'outline'} className='gap-2'>
+    <Button
+      variant={'outline'}
+      className='gap-2'
+      disabled={loading}
+      onClick={() => {
+        startTransition(updateFormContent)
+      }}>
       <HiSaveAs className='h-4 w-4' />
-        Save
+      Save
+      {loading && <FaSpinner className='animate-spin' />}
     </Button>
   )
 }
