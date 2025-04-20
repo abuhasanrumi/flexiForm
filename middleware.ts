3099b8d78a0cd8552e22eaaea96307d538982df6
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2025-04-20 13:41:24 +0600

    feat(middleware): add '/submit/(.*)' to public routes
    Extend the list of public routes to include '/submit/(.*)' to ensure that the submit route is accessible without authentication

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,8 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isPublicRoute = createRouteMatcher([
   '/sign-in(.*)',
-  '/sign-up(.*)'
+  '/sign-up(.*)',
+  '/submit/(.*)'
 ])
 
 export default clerkMiddleware(async (auth, request) => {
@@ -16,6 +17,6 @@ export const config = {
     // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+    '/(api|trpc)(.*)'
+  ]
 }
