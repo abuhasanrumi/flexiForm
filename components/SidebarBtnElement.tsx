eb0d5c8f794c969922a0c2c3e72de763c89341bf
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2025-04-20 12:56:15 +0600

    style(SidebarBtnElement): improve button styling and layout
    Refactor the button styling to enhance visual consistency and responsiveness. Adjust the padding, border, and hover effects to provide a more polished user experience. Update the icon size and text properties for better readability.

--- components/SidebarBtnElement.tsx
+++ components/SidebarBtnElement.tsx
@@ -4,8 +4,12 @@ import { Button } from './ui/button'
 import { useDraggable } from '@dnd-kit/core'
 import { cn } from '@/lib/utils'
 
-export default function SidebarBtnElement({formElement} : {formElement: FormElement}) {
-  const {label, icon: Icon} = formElement.designerBtnElement
+export default function SidebarBtnElement({
+  formElement
+}: {
+  formElement: FormElement
+}) {
+  const { label, icon: Icon } = formElement.designerBtnElement
 
   const draggable = useDraggable({
     id: `designer-btn-${formElement.type}`,
@@ -15,20 +19,35 @@ export default function SidebarBtnElement({formElement} : {formElement: FormElem
     }
   })
   return (
-    <Button ref={draggable.setNodeRef} variant={'outline'} className={cn('flex flex-col gap-2 h-[120px] w-[120px] cursor-grab', draggable.isDragging && "ring-2 ring-primary")} {...draggable.listeners} {...draggable.attributes}>
-      <Icon className="h-8 w-8 text-primary cursor-grab" />
-      <p className='text-xs'>{label}</p>
+    <Button
+      ref={draggable.setNodeRef}
+      variant={'outline'}
+      className={cn(
+        'flex flex-col gap-2 h-[120px] w-auto cursor-grab bg-white hover:bg-zinc-50 border border-zinc-200 rounded-lg transition-colors p-2',
+        'hover:border-primary/30 active:border-primary/50',
+        draggable.isDragging && 'ring-2 ring-primary/50'
+      )}
+      {...draggable.listeners}
+      {...draggable.attributes}>
+      <Icon className='h-12 w-12 text-zinc-700 cursor-grab' />
+      <p className='text-sm text-zinc-600 text-wrap'>{label}</p>
     </Button>
   )
 }
 
-export function SidebarBtnElementDragOverlay({formElement} : {formElement: FormElement}) {
-  const {label, icon: Icon} = formElement.designerBtnElement
+export function SidebarBtnElementDragOverlay({
+  formElement
+}: {
+  formElement: FormElement
+}) {
+  const { label, icon: Icon } = formElement.designerBtnElement
 
   return (
-    <Button variant={'outline'} className='flex flex-col gap-2 h-[120px] w-[120px] cursor-grab'>
-      <Icon className="h-8 w-8 text-primary cursor-grab" />
-      <p className='text-xs'>{label}</p>
+    <Button
+      variant={'outline'}
+      className='flex flex-col gap-2 h-[120px] w-auto cursor-grab bg-white border border-zinc-200 rounded-lg'>
+      <Icon className='h-12 w-12 text-zinc-700 cursor-grab' />
+      <p className='text-sm text-zinc-600'>{label}</p>
     </Button>
   )
 }
